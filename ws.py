6a1de08ad7144cb42fb3b133914fbb2f496d125f
pepeette <59664130+pepeette@users.noreply.github.com>
2023-03-17 16:30:23 +0700

    prgme to run with flask run --port=8000

--- ws.py
+++ ws.py
@@ -5,8 +5,10 @@ import csv
 from time import sleep
 from random import randint
 import pandas as pd
-import nbconvert
-import typer
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 pages = ['', 'xbox-one', 'ps4', '3ds', 'pc', 'nintendo-switch']
@@ -35,8 +37,8 @@ def search_event(pages, headers):
         dates = soup.find_all(class_="media-release-date")
         for d in dates:
             date.append(d.findChild().text)
-        sleep(randint(2,10))
-
+        sleep(randint(2,20))
+    
     return event, date
 
         
@@ -50,7 +52,9 @@ def create_dataframe(event, date):
     df['paged'] = df['event'].astype(str)
     df['paged'] = df['paged'].str.lower()
     df['paged'] = df['paged'].str.replace(":","").str.replace("'","").str.replace(".","").str.replace("&","and").str.replace("- ","")
-    df['paged'] = df['paged'].str.replace(" ","-")
+    df['paged'] = df['paged'].str.replace(" ","-")  
+    df['paged'] = df['paged'].apply(lambda x: x.replace(',', ''))
+    df['paged'] = df['paged'].apply(lambda x: x.replace('!', ''))
     return df
 
 def search_game(df, headers):
@@ -70,24 +74,22 @@ def search_game(df, headers):
     return platform, des
 
 
-def compile(df, platform, des):
+def compile_results(df, platform, des):
     df['platform'] = platform
     df['des'] = des
 
     df.to_csv('eventsTest.csv')
 
-
-def main(param: str):
-    process = Process()
-    process.run()
-    search = search_event(pages, headers)
-    result = create_dataframe(event, date)
-    result = search_game(df, headers)
-    compile(df, platform, des)
-
+@app.route('/process_events', methods=['GET'])
+def process_events():
+    event, date = search_event(pages, headers)
+    df = create_dataframe(event, date)
+    platform, des = search_game(df, headers)
+    compile_results(df, platform, des)
+    # Convert the DataFrame to a JSON object
+    result = df.to_dict(orient='records')
+    # Return the JSON object as the API response
+    return jsonify(result)
 
 if __name__ == "__main__":
-    typer.run(main)
-
-
-
+    app.run(debug=True)
